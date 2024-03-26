92a2a6c6b5f8059828bd028ae289942289482611
turjo997 <turja997@gmail.com>
2024-03-26 13:01:19 +0600

    Wikimedia consumer is added

---
+++ real-world-problem/kafka-consumer-database/src/main/java/com/kafka/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.kafka.springboot.repository;
+
+import com.kafka.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
